06d4c6e2d111bf41c8b7fdd8224a94768a727016
Lucia Englender <luenglender@gmail.com>
2022-04-16 12:26:10 -0300

    deploying

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -5,13 +5,9 @@ const ContactForm = () => {
   const handleSubmit = (event) => {
     event.preventDefault();
 
-    // do your verifications and checks
-    if (!verified) return false;
-
     const REQUEST_PARAMETERS = {
       method: `POST`,
       headers: { "Content-Type": `application/x-www-form-urlencoded` },
-      body: encode({ ...data }), //your data here. Needs to have your form-name attribute set
     };
 
     fetch(`/`, REQUEST_PARAMETERS)
