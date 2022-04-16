ce610828013607c594f399f4b54b068a7484b1be
Lucia Englender <luenglender@gmail.com>
2022-04-16 14:07:56 -0300

    deploying

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -6,8 +6,7 @@ const ContactForm = () => {
     <div className="formTotal">
       <h1>Leave me a message</h1>
       <section className="contact-form">
-        <form name="contact" data-netlify="true" method="POST" id="contact">
-          <input type="hidden" name="contact" value="contact" />
+        <form name="contact-portfolio" data-netlify="true" method="POST">
           <div>
             <div className="nombre">
               <label for="name">
