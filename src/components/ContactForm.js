9e3cbc88d640c5a3d419b29b2797632e1afeb0b0
Lucia Englender <luenglender@gmail.com>
2022-04-16 17:07:03 -0300

    trying to deploy

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -2,23 +2,10 @@ import React from "react";
 import "./ContactForm.css";
 
 const ContactForm = () => {
-  // const handleSubmit = (e) => {
-  //   e.preventDefault();
-  //   let myForm = document.getElementById("forma-contact");
-  //   let formData = new FormData(myForm);
-  //   fetch("/", {
-  //     method: "POST",
-  //     headers: { "Content-Type": "application/x-www-form-urlencoded" },
-  //     body: new URLSearchParams(formData).toString(),
-  //   })
-  //     .then(() => console.log("Form successfully submitted"))
-  //     .catch((error) => alert(error));
-  // };
-
   return (
     <div className="formTotal">
       <h1>Leave me a message</h1>
-      {/* <section className="contact-form">
+      <section className="contact-form">
         <form
           name="contact-portfolio"
           data-netlify="true"
@@ -63,45 +50,11 @@ const ContactForm = () => {
                   placeholder="Anything you want to say"
                 />
               </label>
-              <input
-                type="submit"
-                className="boton-submit"
-                value="Send"
-                
-              />
+              <input type="submit" className="boton-submit" value="Send" />
             </div>
           </div>
         </form>
-      </section> */}
-      <form name="contact" method="POST" data-netlify="true">
-        <p>
-          <label>
-            Your Name: <input type="text" name="name" />
-          </label>
-        </p>
-        <p>
-          <label>
-            Your Email: <input type="email" name="email" />
-          </label>
-        </p>
-        <p>
-          <label>
-            Your Role:{" "}
-            <select name="role[]" multiple>
-              <option value="leader">Leader</option>
-              <option value="follower">Follower</option>
-            </select>
-          </label>
-        </p>
-        <p>
-          <label>
-            Message: <textarea name="message"></textarea>
-          </label>
-        </p>
-        <p>
-          <button type="submit">Send</button>
-        </p>
-      </form>
+      </section>
     </div>
   );
 };
