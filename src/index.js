c231b4d41c6b13fef9306c6b9adf1a326ba3b9c8
Lucia Englender <luenglender@gmail.com>
2022-04-16 18:59:37 -0300

    trying to deploy

--- src/index.js
+++ src/index.js
@@ -1,13 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { HashRouter } from "react-router-dom";
 import App from "./App";
 
 ReactDOM.render(
-  <HashRouter>
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
-  </HashRouter>,
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
   document.getElementById("root")
 );
