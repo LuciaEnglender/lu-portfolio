9bf93dd05d6a4d89b4de20be29d1a008f653e456
Lucia Englender <luenglender@gmail.com>
2022-04-16 17:03:49 -0300

    trying to deploy

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -18,7 +18,7 @@ const ContactForm = () => {
   return (
     <div className="formTotal">
       <h1>Leave me a message</h1>
-      <section className="contact-form">
+      {/* <section className="contact-form">
         <form
           name="contact-portfolio"
           data-netlify="true"
@@ -67,12 +67,41 @@ const ContactForm = () => {
                 type="submit"
                 className="boton-submit"
                 value="Send"
-                onSubmit={handleSubmit}
+                
               />
             </div>
           </div>
         </form>
-      </section>
+      </section> */}
+      <form name="contact" method="POST" data-netlify="true">
+        <p>
+          <label>
+            Your Name: <input type="text" name="name" />
+          </label>
+        </p>
+        <p>
+          <label>
+            Your Email: <input type="email" name="email" />
+          </label>
+        </p>
+        <p>
+          <label>
+            Your Role:{" "}
+            <select name="role[]" multiple>
+              <option value="leader">Leader</option>
+              <option value="follower">Follower</option>
+            </select>
+          </label>
+        </p>
+        <p>
+          <label>
+            Message: <textarea name="message"></textarea>
+          </label>
+        </p>
+        <p>
+          <button type="submit">Send</button>
+        </p>
+      </form>
     </div>
   );
 };
