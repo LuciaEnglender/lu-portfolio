c2e68084061b3305fa20ef8852a1da67de4349df
Lucia Englender <luenglender@gmail.com>
2022-04-16 12:24:16 -0300

    deploying

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -2,11 +2,36 @@ import React from "react";
 import "./ContactForm.css";
 
 const ContactForm = () => {
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    // do your verifications and checks
+    if (!verified) return false;
+
+    const REQUEST_PARAMETERS = {
+      method: `POST`,
+      headers: { "Content-Type": `application/x-www-form-urlencoded` },
+      body: encode({ ...data }), //your data here. Needs to have your form-name attribute set
+    };
+
+    fetch(`/`, REQUEST_PARAMETERS)
+      .then(() => {
+        console.log(`OK`);
+      })
+      .catch((error) => alert(error));
+  };
   return (
     <div className="formTotal">
       <h1>Leave me a message</h1>
       <section className="contact-form">
-        <form name="contact" netlify data-netlify="true" method="POST">
+        <form
+          name="contact"
+          netlify
+          data-netlify="true"
+          method="POST"
+          data-netlify-honeypot="bot-field"
+          data-netlify-recaptcha="true"
+        >
           <input type="hidden" name="contact" value="contact" />
           <div>
             <div className="nombre">
@@ -40,7 +65,12 @@ const ContactForm = () => {
                 rows="10"
                 placeholder="Anything you want to say"
               />
-              <input type="submit" className="boton-submit" value="Send" />
+              <input
+                type="submit"
+                className="boton-submit"
+                value="Send"
+                onClick={handleSubmit}
+              />
             </div>
           </div>
         </form>
