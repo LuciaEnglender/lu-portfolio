b34043e6f55f50374c84906bbf5168ceb636e93f
Lucia Englender <luenglender@gmail.com>
2022-04-18 11:08:16 -0300

    trying to deploy

--- src/App.js
+++ src/App.js
@@ -2,18 +2,18 @@ import "./App.css";
 import React from "react";
 import Home from "./components/Home";
 import { Route } from "react-router-dom";
-import { BrowserRouter } from "react-router-dom";
 import { Switch } from "react-router-dom";
 import ThankYou from "./components/ThankYou";
+import { HashRouter } from "react-router-dom";
 
 function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Switch>
         <Route path="/" render={() => <Home />} />
         <Route path="/thanks" render={() => <ThankYou />} />
       </Switch>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
