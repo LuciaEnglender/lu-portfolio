e294dfa527ac9e965f6c1ec76fa87c04a2499abb
Lucia Englender <luenglender@gmail.com>
2022-05-18 22:09:38 -0300

    cover correction

--- src/components/Cover.js
+++ src/components/Cover.js
@@ -1,14 +1,34 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./Cover.css";
 import coverVideo from "../media/coverVideo.mp4";
 const Cover = () => {
+  let [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setIsLoading(false);
+    }, 1000);
+  }, []);
+
   return (
-    <div className="cover-container">
-      <video className="video" src={coverVideo} autoPlay loop muted />
-      <div className="textoCover">
-        <h1>Lucia Englender</h1>
-        <p>👩‍💻 Full Stack Developer 👩‍💻</p>
-      </div>
+    <div>
+      {isLoading ? (
+        <div className="cover-container-loading">
+          <video className="video" src={coverVideo} autoPlay loop muted />
+          <div className="textoCover">
+            <h1>Lucia Englender</h1>
+            <p>👩‍💻 Full Stack Developer 👩‍💻</p>
+          </div>
+        </div>
+      ) : (
+        <div className="cover-container">
+          <video className="video" src={coverVideo} autoPlay loop muted />
+          <div className="textoCover">
+            <h1>Lucia Englender</h1>
+            <p>👩‍💻 Full Stack Developer 👩‍💻</p>
+          </div>
+        </div>
+      )}
     </div>
   );
 };
